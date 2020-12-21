eb6bc524ab540161feed7a7de50b35bc8dcb374c
杨万浩 <yangwanhao163@163.com>
2020-12-21 11:47:10 +0800

    版本修改&测试代码修改

--- test/src/test/java/cn/yangwanhao/sdk/test/TestApplicationTests.java
+++ test/src/test/java/cn/yangwanhao/sdk/test/TestApplicationTests.java
@@ -1,5 +1,7 @@
 package cn.yangwanhao.sdk.test;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
+import cn.yangwanhao.sdk.common.component.ValidateComponent;
 import cn.yangwanhao.sdk.test.common.dto.ValidateDto;
 
 @SpringBootTest
@@ -15,14 +18,13 @@ import cn.yangwanhao.sdk.test.common.dto.ValidateDto;
 public class TestApplicationTests {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private ValidateComponent validateComponent;
 
     @Test
     public void validateTest() {
         ValidateDto validateDto = new ValidateDto();
-        ResponseEntity<String> res = restTemplate.postForEntity("http://localhost:8080/validate/test",
-           validateDto, String.class);
-        System.err.println(res.getBody());
+        List<String> errorMessages = validateComponent.validateAll(validateDto);
+        System.err.println(errorMessages);
     }
 
 }
